fc99fe067897e6be363486710d952baeaf086cd6
Pranav Ladole <pranavladole98@gmail.com>
2024-01-15 20:37:01 +0530

    arr length

--- arrays/Simple.java
+++ arrays/Simple.java
@@ -1,5 +1,10 @@
 public class Simple{
     public static void main(String args[]){
-        System.out.printf("simple 123");
+        int[] age = new int[2];
+        age[0] = 1;
+        age[1]=2;
+        for (int j=0;j<age.length;j++){
+            System.out.println("age " +age[j]);
+        }
     }
 }
